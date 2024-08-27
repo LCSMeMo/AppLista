e2906b406c65507be1d977edb1a6a0d6dc2faf48
LCSMeMo <lucasmenezesdemoura1@gmail.com>
2024-08-27 10:51:51 -0400

    controller salvar dados - MVC

--- app/src/main/java/devandroid/lcsmemo/applista/controller/UsuarioController.java
+++ app/src/main/java/devandroid/lcsmemo/applista/controller/UsuarioController.java
@@ -1,4 +1,22 @@
 package devandroid.lcsmemo.applista.controller;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import devandroid.lcsmemo.applista.model.Usuario;
+
 public class UsuarioController {
+
+
+    @NonNull
+    @Override
+    public String toString() {
+        Log.d("CONTROLLER_MVC", "CONTROLLER INICIADA... ");
+        return super.toString();
+    }
+
+    public void salvar(Usuario usuario) {
+        Log.d("CONTROLLER_MVC", "DADOS SALVOS: "+usuario.toString());
+    }
 }
